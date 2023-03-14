44d34e1e935285639e33aeb19d85d4b5c69902ce
Daniel Choi <schoiak@connect.ust.hk>
2023-03-14 16:31:15 +0900

    Mod - converted to ArrayList

--- HeadFirstJava/src/com/exercise/ch4/lifeanddeath/DogTestDrive.java
+++ HeadFirstJava/src/com/exercise/ch4/lifeanddeath/DogTestDrive.java
@@ -1,10 +1,18 @@
 package com.exercise.ch4.lifeanddeath;
 
+import java.util.ArrayList;
+
 public class DogTestDrive {
 	public static void main(String[] arge) {
 		Dog arrow = DogFactory.comeIntoBeing();
 		arrow.setSize(10);
 		arrow.bark();
+		
+
+		ArrayList cars = new ArrayList();
+		cars.add("Volvo");
+		cars.add("BMW");
+		cars.add("Ford");
 	}
 }
 
